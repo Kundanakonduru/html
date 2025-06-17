b1230991fb3d515e59abcfdc5c39e9f5a42a8387
Kundanakonduru <kondurukundana1@gmail.com>
2025-06-17 20:01:07 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+class DataTypes
+{
+public static void main(String[] args)
+{
+int age=25;
+boolean isStudent=true;
+char grade='S';
+float height=5.0f;
+double weight=55.0;
+System.out.println("age:"+age);
+System.out.println("isStudent:"+isStudent);
+System.out.println("grade:"+grade);
+System.out.println("height:"+height);
+System.out.println("weight:"+weight);
+}
+}
