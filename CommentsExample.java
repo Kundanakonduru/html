63671488205313256a523042315ba417ae55a66b
Kundanakonduru <kondurukundana1@gmail.com>
2025-06-17 19:49:27 +0530

    Create CommentsExample.java

---
+++ CommentsExample.java
@@ -0,0 +1,16 @@
+/**
+ * This is a documentation comment
+ * Author: Lavanya
+ */
+public class CommentsExample {
+public static void main(String[] args) {
+// This is a single-line comment
+System.out.println("Hello");
+
+/*
+ * This is a multi-line comment
+* Printing Welcome
+*/
+System.out.println("Welcome");
+}
+}
