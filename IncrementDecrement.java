ddc2bd0245b54e9ae3bd21575a6116f2d8fb556c
Kundanakonduru <kondurukundana1@gmail.com>
2025-06-17 20:21:34 +0530

    Create IncrementDecrement.java

---
+++ IncrementDecrement.java
@@ -0,0 +1,12 @@
+class IncrementDecrement
+{
+public static void main(String[] args)
+{
+int a=10;
+System.out.println("Original:"+a);
+a++;
+System.out.println("Incremented:"+a);
+a--;
+System.out.println("Decrement:"+a);
+}
+}
